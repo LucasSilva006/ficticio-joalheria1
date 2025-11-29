5340f435d29a841e89c554b57947c3639a6dbc4d
Lucas Silva <155262753+LucasSilva006@users.noreply.github.com>
2025-11-29 19:48:08 -0300

    Atualização na home
    Aumentar h1 e p da hero section
    Aumentar p do copy
    Consertar menu hamburguer
    Aumentar li dos links rápidos e p dos contatos
    Scroll margin das seções (e aumentar o da seção destaque)

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,13 @@
 function mostrarMenu() {
-    const navMenu = document.querySelector('nav');
-    navMenu.classList.toggle('menu-navegacao-disabled');
+    const navMenu = document.querySelector('.menu-sidebar');
+    const overlay = document.querySelector('.overlay-menu');
+    navMenu.classList.add('menu-aberto');
+    overlay.classList.add('overlay-visivel');
+}
+
+function fecharMenu() {
+    const navMenu = document.querySelector('.menu-sidebar');
+    const overlay = document.querySelector('.overlay-menu');
+    navMenu.classList.remove('menu-aberto');
+    overlay.classList.remove('overlay-visivel');
 }
